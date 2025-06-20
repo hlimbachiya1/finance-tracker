c1ec957abf6ccb2fc2c55bb17077f3434ca6df49
Hemang Limbachiya <hlimbachiya@hawk.iit.edu>
2025-06-20 14:32:34 -0500

    updated Header with React Router navigation + styling

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,4 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 function Header() {
+
+  const navStyle = {
+    color: '#007bff',
+    textDecoration: 'none',
+    padding: '8px 16px',
+    borderRadius: '5px',
+    margin: '0 5px',
+    border: '1px solid transparent',
+    transition: 'all 0.2s ease'
+  };
+
+    const activeNavStyle = {
+    ...navStyle,
+    backgroundColor: '#007bff',
+    color: 'white',
+    border: '1px solid #007bff'
+  };
+
   return (
     <header style={{ 
             borderBottom: '2px solid #ccc', 
@@ -6,12 +26,35 @@ function Header() {
             marginBottom: '20px' }}>
 
       <h1>Finance-Tracker</h1>
+
+      <nav>
+        <NavLink
+        to = "/"
+        style={({isActive}) => isActive ? activeNavStyle : navStyle}
+        >
+          Dashboard
+        </NavLink>
+
+        <NavLink
+        to = "/add"
+        style={({isActive}) => isActive ? activeNavStyle : navStyle}
+        >
+          + Add Transaction
+        </NavLink>
+
+        <NavLink
+        to = "/about"
+        style={({isActive}) => isActive ? activeNavStyle : navStyle}
+        >
+          About
+        </NavLink>
+      </nav>
       
-      <nav style={{ marginTop: '10px' }}>
+      {/* <nav style={{ marginTop: '10px' }}>
         <button style={{ marginRight: '10px' }}>Dashboard</button>
         <button style={{ marginRight: '10px' }}>Add Transaction</button>
         <button>About</button>
-      </nav>
+      </nav> */}
     </header>
   );
 }
